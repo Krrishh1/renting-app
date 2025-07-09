4e0d0b7b2babd08d264effbbb65c483362253bde
krishna Rajput <krishnarajput4045@gmail.com>
2025-07-09 18:33:33 +0530

    Update AuthContext.jsx

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from 'react'
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    const serverUrl = "http://localhost:8000"
+    const serverUrl = "https://renting-app-backend.onrender.com"
 
     let [loading,setLoading]=useState(false)
 
