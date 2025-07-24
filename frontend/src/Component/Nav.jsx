0ebd08bccc6181f0e26ae3fc865f42596cbddfd1
krishna Rajput <krishnarajput4045@gmail.com>
2025-07-25 02:52:32 +0530

    Update Nav.jsx

--- frontend/src/Component/Nav.jsx
+++ frontend/src/Component/Nav.jsx
@@ -70,7 +70,7 @@ function Nav() {
     <input 
       type="text" 
       className='w-full pl-4 pr-12 py-2.5 border-2 border-gray-300 rounded-full text-base outline-none transition-colors focus:border-red-500' 
-      placeholder='Search anywhere...'
+      placeholder='Search property or place'
       onChange={(e) => setInput(e.target.value)} 
       value={input}
     />
@@ -82,7 +82,7 @@ function Nav() {
   </div>
 </div>
                 <div className='flex items-center justify-center    gap-[10px] relative'>
-                    <span className='text-[18px] cursor-pointer rounded-[50px] hover:bg-[#ded9d9] px-[8px] py-[5px] hidden md:block' onClick={()=>navigate("/listingpage1")}>List your home</span>
+                    <span className='text-[18px] cursor-pointer rounded-[50px] hover:bg-[#ded9d9] px-[8px] py-[5px] hidden md:block' onClick={()=>navigate("/listingpage1")}>List your Home</span>
                     <button className='px-[20px] py-[10px] flex items-center justify-center gap-[5px] border-[1px] border-[#8d8c8c] rounded-[50px] hover:shadow-lg' onClick={()=>setShowpopup(prev =>!prev)}>
                         <span><GiHamburgerMenu className='w-[20px] h-[20px]' /></span>
                         {userData == null && <span><CgProfile className='w-[23px] h-[23px]' /></span>}
@@ -123,7 +123,7 @@ function Nav() {
             <div className='w-[100%] h-[60px] flex items-center justify-center  md:hidden 
             '>
                 <div className='w-[80%] relative '>
-                    <input type="text" className='w-[100%] px-[30px] py-[10px] border-[2px] border-[#bdbaba] outline-none overflow-auto rounded-[30px] text-[17px]' placeholder='Any Where  |  Any Location  |  Any City ' onChange={(e)=>setInput(e.target.value)} value={input} />
+                    <input type="text" className='w-[100%] px-[30px] py-[10px] border-[2px] border-[#bdbaba] outline-none overflow-auto rounded-[30px] text-[17px]' placeholder='Search property or place' onChange={(e)=>setInput(e.target.value)} value={input} />
                     <button className='absolute p-[10px] rounded-[50px] bg-[red] right-[3%] top-[5px]'><FiSearch className='w-[20px] h-[20px] text-[white]' /></button>
                 </div>
                 </div>
